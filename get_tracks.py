600a0b0528c9a263a00db631eb217060d4b954c7
Bobby <steinbach.rj@gmail.com>
2014-01-25 02:33:38 -0800

    fixes

--- get_tracks.py
+++ get_tracks.py
@@ -1,16 +1,30 @@
 import soundcloud
+from lib import vlc
 
-def get_songs():
-    CLIENT_ID = '7d365fefd91122c615f4ebbe66f512b1'
-    CLIENT_SECRET = 'cde1a5fde5cb20712dda2070f852d5dc'
-    client = soundcloud.Client(client_id=CLIENT_ID)
-    
-    tracks = client.get('/tracks', limit=10)
-    for track in tracks:
-        print track.stream_url
+class Controller():
+    def __init__(self):
+        self.vlc = vlc.Instance()
+
+    def get_songs(self):
+        CLIENT_ID = '7d365fefd91122c615f4ebbe66f512b1'
+        CLIENT_SECRET = 'cde1a5fde5cb20712dda2070f852d5dc'
+        client = soundcloud.Client(client_id=CLIENT_ID)
         
-def play_songs(songs):
-    pass
+        tracks = client.get('/tracks', limit=10)
+        urls = []
+        for track in tracks:
+            url_to_play = '{url}?consumer_key={key}'.format(url=track.stream_url, key=CLIENT_ID)
+            urls.append(url_to_play)
+            print url_to_play
+            
+        return urls
+              
+    def play_songs(self, song_urls):
+        media_list = self.vlc.media_list_new(mrls=song_urls)
+        media_player = self.vlc.media_list_player_new()
+        media_player.set_media_list(media_list)
+        media_player.play()
+   
 
-songs = get_songs()
-play_songs(songs)
+controller = Controller()
+controller.play_songs(controller.get_songs())
