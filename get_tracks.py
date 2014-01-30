4d296f00871c250dea5d6d8c234ab267e75d0da1
Bobby <steinbach.rj@gmail.com>
2014-01-29 23:49:29 -0800

    changes'

--- get_tracks.py
+++ get_tracks.py
@@ -1,26 +1,43 @@
 import soundcloud
 import logging
 from Tkinter import *
+from PIL import Image, ImageTk
 from lib import vlc
+from submodules.eight_py.eight_py import Api
 import pdb
 
 
+EIGHT_TRACKS = "8Tracks"
+SOUNDCLOUD = "Soundcloud"
+
 class Controller():
     def __init__(self):
-        self.vlc = vlc.Instance()
+        self.vlc = vlc.Instance()        
         self.media_player = self.vlc.media_list_player_new()   
         self.media = None     
+        self.media_source = SOUNDCLOUD
+        # string vars set by Window instance
+        self.info_var = None
+        self.filter_var = None
 
         logging.basicConfig(level=logging.DEBUG, format='%(levelname)s:%(message)s')
 
-    def get_songs(self, **kwargs):
+    def set_media_source(self, new_source):
+        self.media_source = new_source
+
+    def add_track_info(self, track_name):
+        """
+        add a new track to this controller's string var which is displaying all song info
+        """
+        self.info_var.set('{prev} \n {cur}'.format(prev=self.info_var.get().encode('utf-8'), cur=track_name.encode('utf-8')))
+
+    def get_soundcloud_songs(self):
         CLIENT_ID = '7d365fefd91122c615f4ebbe66f512b1'
         CLIENT_SECRET = 'cde1a5fde5cb20712dda2070f852d5dc'
         client = soundcloud.Client(client_id=CLIENT_ID)
 
-        filter_val = kwargs.get('q', None)
-        song_info = kwargs.get('info')
-        song_info.set('\n\n Now playing: \n')
+        filter_val = self.filter_var.get()
+        self.info_var.set('\n\n Now playing Soundcloud Songs: \n')
         
         logging.info('getting tracks from soundcloud')
         tracks = client.get('/tracks', q=filter_val, limit=10) if filter_val else client.get('/tracks', limit=20)
@@ -29,22 +46,46 @@ class Controller():
             if track.streamable:
                 logging.info('adding streamable url for track {name} with url {url}'.format(name=track.title.encode('utf-8'),  url=track.stream_url.encode('utf-8')))
                 url_to_play = '{url}?consumer_key={key}'.format(url=track.stream_url, key=CLIENT_ID)
-                song_info.set('{prev} \n {cur}'.format(prev=song_info.get().encode('utf-8'), cur=track.title.encode('utf-8')))
+                self.add_track_info(track.title)
             urls.append(url_to_play)
             
         return urls
 
-    def get_8tracks_songs(self):
-        API_KEY = '7fe2e057bb81abf2248a06ecab027b8dc09e01d3.'
+    def get_8tracks_songs(self):   
+        """
+        get a song from 8tracks (since we cant just get a bunch of songs at once like we can for soundcloud)
+        @return a list of song urls to stream
+        """
+        logging.info('getting tracks from 8tracks')
+        API_KEY = '7fe2e057bb81abf2248a06ecab027b8dc09e01d3'        
+        self.info_var.set('\n\n Now playing 8Tracks Songs: \n')
 
-    def play_songs(self, song_urls):
-        logging.info('getting ready to play the tracks')
+        api = Api(api_key=API_KEY)
+        mixes = api.get_mixes(num_results=1)
+        track = api.start_playback(mixes[0])        
+
+        urls = []
+        while not track['done']:
+            urls.append(track['stream_url'])
+            self.add_track_info('{name} by {performer}'.format(name=track['name'], performer=track['performer']))
+            track = api.next_song()            
+        logging.info('got track {track}'.format(track=track))
+
+        return urls 
+
+    def play_songs(self):        
+        logging.info('getting song urls to play the tracks')
+        song_urls = self.get_soundcloud_songs() if self.media_source == SOUNDCLOUD else self.get_8tracks_songs()
+
         if self.media:
+            logging.info('there was already media, releasing it and stopping the playlist so we can load the new media')
             self.media.release()
-        self.media = self.vlc.media_list_new(mrls=song_urls)        
+            self.stop()
 
-        self.media_player.set_media_list(self.media)
+        self.media = self.vlc.media_list_new(mrls=song_urls)
+        self.media_player.set_media_list(self.media)         
         self.media_player.play()
+
         logging.info('now playing')
 
     def pause(self):
@@ -63,15 +104,8 @@ class Controller():
         logging.info('going to next track')
         self.media_player.previous()
 
-    def filter_songs(self, filter_val, info_var):
-        logging.info('going to filter songs now')
-        song_urls = self.get_songs(q=filter_val, info=info_var)
-        self.stop()
-        self.play_songs(song_urls)
-
     def download(self):
         pass
-
 
 
 class Window():
@@ -82,32 +116,55 @@ class Window():
         self.setup_widgets(top)       
         top.mainloop() 
 
-    def setup_widgets(self, window):        
+    def setup_widgets(self, window):    
+        ##--< Frames >--##
+        top_frame = Frame(window)
+        top_frame.pack(side=TOP)
+
+        bottom_frame = Frame(window)
+        bottom_frame.pack(side=BOTTOM)
+
         ##--< Labels >--##
         info_string_var = StringVar()
-        info_label = Label(window, textvariable=info_string_var)
-        info_label.grid(row=2, column=2)
+        info_label = Label(bottom_frame, textvariable=info_string_var)
+        info_label.pack(side=LEFT)
+        self.controller.info_var = info_string_var
 
+        ##--< Radio Buttons >--##    
+        eight_raw_image = Image.open("media/8tracks.jpg")    
+        eight_raw_image = eight_raw_image.resize((100, 100), Image.ANTIALIAS)
+        eight_image = ImageTk.PhotoImage(eight_raw_image)
+        eight_button = Radiobutton(top_frame, image=eight_image, command=lambda: self.controller.set_media_source(EIGHT_TRACKS))
+        eight_button.image = eight_image
+        eight_button.grid(row=0, column=0)
+
+        soundcloud_raw_image = Image.open("media/soundcloud.jpg")
+        soundcloud_raw_image = soundcloud_raw_image.resize((100, 100), Image.ANTIALIAS)
+        soundcloud_image = ImageTk.PhotoImage(soundcloud_raw_image)
+        soundcloud_button = Radiobutton(top_frame, image=soundcloud_image, command=lambda: self.controller.set_media_source(SOUNDCLOUD))
+        soundcloud_button.image = soundcloud_image
+        soundcloud_button.grid(row=0, column=1)
         ##--< Buttons >--##
-        play_button = Button(window, text="Play", command=lambda: self.controller.play_songs(self.controller.get_songs(info=info_string_var)))
-        play_button.grid(row=0, column=0)
+        play_button = Button(top_frame, text="Play", command=lambda: self.controller.play_songs())
+        play_button.grid(row=1, column=0)
 
-        pause_button = Button(window, text="Pause", command=self.controller.pause)
-        pause_button.grid(row=0, column=1)
+        pause_button = Button(top_frame, text="Pause", command=self.controller.pause)
+        pause_button.grid(row=1, column=1)
 
-        previous_button = Button(window, text="Previous", command=self.controller.previous)
-        previous_button.grid(row=0, column=2)
+        previous_button = Button(top_frame, text="Previous", command=self.controller.previous)
+        previous_button.grid(row=1, column=2)
 
-        next_button = Button(window, text="Next", command=self.controller.next)
-        next_button.grid(row=0, column=3)
+        next_button = Button(top_frame, text="Next", command=self.controller.next)
+        next_button.grid(row=1, column=3)
 
         filter_string_var = StringVar()
-        filter_button = Button(window, text="Filter", command=lambda: self.controller.filter_songs(filter_string_var.get(), info_string_var))
-        filter_button.grid(row=1, column=0)
+        filter_button = Button(top_frame, text="Filter", command=lambda: self.controller.play_songs())
+        filter_button.grid(row=2, column=0)
+        self.controller.filter_var = filter_string_var
 
         ##--< Entries >--##
-        filter_entry = Entry(window, bd=5, textvariable=filter_string_var)
-        filter_entry.grid(row=1, column=1)
+        filter_entry = Entry(top_frame, bd=5, textvariable=filter_string_var)
+        filter_entry.grid(row=2, column=1)
 
 
 Window()
